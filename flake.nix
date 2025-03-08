2ac2f588c954ac4f9412d78836f78bd9689d721f
Numb-0 <cosimofalla@gmail.com>
2025-03-08 21:47:36 +0100

    update astal

--- flake.nix
+++ flake.nix
@@ -1,10 +1,6 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    astal = {
-      url = "github:aylur/astal";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     ags = {
       url = "github:aylur/ags";
       inputs.nixpkgs.follows = "nixpkgs";
